70399f081e62106ccc267fb96ad7d27ecd5686eb
Jim <limchorngjim@gmail.com>
2022-11-15 16:28:00 +0100

    removed for loop

--- helper.js
+++ helper.js
@@ -7,23 +7,20 @@ export const searchArtist = async (artistName) => {
     const url = `https://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${artistName}&api_key=${process.env.LAST_FM_API_KEY}&format=json`;
     const res = await fetch(url);
     const data = await res.json();
-    const artists = data.results.artistmatches.artist;
-
+    
     let artistInfo = {};
-    for (const artist of artists) {
-      if (artist.name.toLowerCase() === artistName.toLowerCase()) {
-        artistInfo.name = artist.name;
-		    artistInfo.mbid = artist.mbid;
-		    artistInfo.url = artist.url;
-        artist.image.forEach((img) => {
-          if (img.size === 'small') {
-            artistInfo.image_small = img['#text'];
-          } else if (img.size === 'medium') {
-            artistInfo.image = img['#text'];
-          }
-		    });
-        break;
-      }
+    if (Object.keys(data).length !== 0) {
+      const artist = data.results.artistmatches.artist[0];
+      artistInfo.name = artist.name;
+      artistInfo.mbid = artist.mbid;
+      artistInfo.url = artist.url;
+      artist.image.forEach((img) => {
+        if (img.size === 'small') {
+          artistInfo.image_small = img['#text'];
+        } else if (img.size === 'medium') {
+          artistInfo.image = img['#text'];
+        }
+      });
     }
     return [artistInfo];
   } catch (e) {
